45d37f97659c02719f5db7608c9d69105ba611f3
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-01-06 11:41:30 +0300

    refactoring

--- parser.py
+++ parser.py
@@ -1,49 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
-import re
-import unicodedata
+# import re
+# import unicodedata
 
 # занести в словарь номер задания, вопрос, текст и ответ
 
+
+all_tasks = []
 url = "https://rus-ege.sdamgia.ru/test?theme=289"
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "lxml")
+for task_id in soup.find_all("div", {"class": "problem_container"}, id=True):
+    all_tasks.append(task_id.get("id").replace("problem_", ""))
 
-# headers_list = []
-heads_result = []
-texts_result = []
-answers_result = []
-# for headers in soup.find_all("p"):
-#     headers_list.append(headers.get("left_margin"))
-# task = soup.find("div", {"class": "prob_view"})
-heads = soup.find_all("div", {"class": "pbody"}, id=True)
-head_counter = 0
-text_counter = 0
-answer_counter = 0
-for head in heads:
-    head_counter += 1
-    head_line = str(head_counter) + " " + head.find('p', {'class': 'left_margin'}).get_text()
-    heads_result.append(head_line)
-texts = soup.find_all("div", {"class": "probtext"})
-for text in texts:
-    text_counter += 1
-    text_line = text.find('p', {'class': 'left_margin'}).get_text()
-    texts_result.append(text_line)
-answers = soup.find_all("div", {"class": "answer"})
-for answer in answers:
-    answer_counter += 1
-    answer_line = str(answer_counter) + " " + answer.find('span').get_text() + " "
-    answers_result.append(answer_line)
-heads_str = ''.join(heads_result)
-texts_str = ''.join(texts_result)
-answers_str = ''.join(answers_result)
-content = {
-    "heads": heads_str,
-    "texts": texts_str,
-    "answers": answers_str
-}
-# corrected_head = str(head).replace("</p>", "")
-# result_heads.append(task)
+print(all_tasks)
+number = 0
+for task in all_tasks:
+    current_id = "sol" + str(task)
+    r = requests.get("https://rus-ege.sdamgia.ru/problem?id=" + str(task))
+    soup = BeautifulSoup(r.text, "lxml")
+    number += 1
+    head = soup.find("div", {"class": "pbody"}).get_text()
+    text = soup.find("div", {"class": "probtext"}).get_text()
+    answer = soup.find("div", {"class": "solution"}, id=current_id).get_text()
+    content = {
+        "number": number,
+        "head": head,
+        "text": text,
+        "answer": answer
+    }
 
-# print(content)
-print(content)
+    print(content)
