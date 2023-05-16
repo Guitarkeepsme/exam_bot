1e8e86303bdf2dde9b78fcaa78ba3cda2c834f9e
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-05-16 14:27:59 +0300

    saved in file

--- chromedriver/links_for_parsing.py
+++ chromedriver/links_for_parsing.py
@@ -1,26 +1,38 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from fake_useragent import UserAgent
-# from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
-url = "https://rus-ege.sdamgia.ru"
+# url = "https://rus-ege.sdamgia.ru"
 
-user_agent = UserAgent()
-options = webdriver.ChromeOptions()
-options.add_argument(f"user-agent={user_agent.random}")
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--incognito')
-options.add_argument('--headless')
-options.add_argument('--disable-blink-features=AutomationControlled')
-driver = webdriver.Chrome(
-    executable_path="/Users/alexeykashurnikov/PycharmProjects/exam_bot/chromedriver/chromedriver",
-    options=options
-)
+# user_agent = UserAgent()
+# options = webdriver.ChromeOptions()
+# options.add_argument(f"user-agent={user_agent.random}")
+# options.add_argument('--ignore-certificate-errors')
+# options.add_argument('--incognito')
+# options.add_argument('--headless')
+# options.add_argument('--disable-blink-features=AutomationControlled')
+# driver = webdriver.Chrome(
+# ChromeDriverManager().install(), options=options)
+#     executable_path="/Users/alexeykashurnikov/PycharmProjects/exam_bot/chromedriver/chromedriver",
+#     options=options
+# )
+# driver.get(url)
+# html = driver.page_source
+
+
+# with open('links_page.html', 'w') as file:
+#     file.write(html)
+
 links = []
-driver.get(url)
-html = driver.page_source
-soup = BeautifulSoup(html, "lxml")
 
+
+with open('links_page.html') as file:
+    src = file.read()
+
+
+soup = BeautifulSoup(src, "lxml")
 for task in soup.find_all("div", {"class": "ConstructorForm-TopicDesc"}):
     print(task)
     result = task.get('href')
