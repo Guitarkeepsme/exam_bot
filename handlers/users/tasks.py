a8b06d618ce9750f03c83b0f357eccfe9bd17a90
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-06-28 09:38:34 +0300

    first buttons

--- handlers/users/tasks.py
+++ handlers/users/tasks.py
@@ -1,8 +1,21 @@
+import logging
+
+from keyboards.inline.choise_buttons import choice
 from loader import dp
 from aiogram.dispatcher.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 
-@dp.message_handler(Command('options'))
+@dp.message_handler(Command('start'))
 async def show_options(message: Message):
-    await message.answer(text="Для начала выберите предмет, по которому вы хотите позаниматься.", reply_markup=choice)
+    await message.answer(text="Для начала выберите предмет, к которому вы будете готовиться.", reply_markup=choice)
+
+
+@dp.callback_query_handler(text_contains="russian")
+async def choosing_russian(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+
+    await call.message.answer("Этот раздел пока *в разработке*. Вы можете готовиться к другим предметам.",
+                              parse_mode="Markdown")
--- keyboards/inline/choise_buttons.py
+++ keyboards/inline/choise_buttons.py
@@ -1,13 +1,24 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from .callback_data import subject_callback
 
 choice = InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            InlineKeyboardButton(text='Русский язык', callback_data=''),
-            InlineKeyboardButton(text='Математика', callback_data=''),
+            InlineKeyboardButton(text='Русский язык', callback_data=subject_callback.new(subject='russian')),
+            InlineKeyboardButton(text='Математика', callback_data="choose:math"),
+            # InlineKeyboardButton(text='Обществознание', callback_data=''),
+            # InlineKeyboardButton(text='Информатика', callback_data=''),
+            # InlineKeyboardButton(text='Биология', callback_data=''),
+            # InlineKeyboardButton(text='Физика', callback_data=''),
+            # InlineKeyboardButton(text='История', callback_data=''),
+            # InlineKeyboardButton(text='Иностранный язык', callback_data=''),
+            # InlineKeyboardButton(text='Химия', callback_data=''),
+            # InlineKeyboardButton(text='Литература', callback_data=''),
+            # InlineKeyboardButton(text='География', callback_data='')
         ],
         [
-            InlineKeyboardButton(text='Посмотреть свою статистику', callback_data='')
+            InlineKeyboardButton(text='Посмотреть свою статистику', callback_data='next')
         ]
     ]
 )
+
