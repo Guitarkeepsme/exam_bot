0e657a3d0d2f0cf1755ee9f45a567ca283ad2977
Алексей Кашурников <alexeykashurnikov@gmail.com>
2023-06-28 10:33:11 +0300

    subject keyboard

--- handlers/users/tasks.py
+++ handlers/users/tasks.py
@@ -8,7 +8,117 @@ from aiogram.types import Message, CallbackQuery
 
 @dp.message_handler(Command('start'))
 async def show_options(message: Message):
-    await message.answer(text="Для начала выберите предмет, к которому вы будете готовиться.", reply_markup=choice)
+    await message.answer(text="Для начала выберите предмет, к которому вы будете готовиться:", reply_markup=choice)
+
+
+@dp.callback_query_handler(text_contains="russian")
+async def choosing_russian(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+
+    await call.message.answer("Этот раздел пока *в разработке*. Вы можете готовиться к другим предметам.",
+                              parse_mode="Markdown")
+
+
+@dp.callback_query_handler(text_contains="maths")
+async def choosing_russian(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+
+    await call.message.answer("Этот раздел пока *в разработке*. Вы можете готовиться к другим предметам.",
+                              parse_mode="Markdown")
+
+
+@dp.callback_query_handler(text_contains="")
+async def choosing_russian(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+
+    await call.message.answer("Этот раздел пока *в разработке*. Вы можете готовиться к другим предметам.",
+                              parse_mode="Markdown")
+
+
+@dp.callback_query_handler(text_contains="russian")
+async def choosing_russian(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+
+    await call.message.answer("Этот раздел пока *в разработке*. Вы можете готовиться к другим предметам.",
+                              parse_mode="Markdown")
+
+
+@dp.callback_query_handler(text_contains="russian")
+async def choosing_russian(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+
+    await call.message.answer("Этот раздел пока *в разработке*. Вы можете готовиться к другим предметам.",
+                              parse_mode="Markdown")
+
+
+@dp.callback_query_handler(text_contains="russian")
+async def choosing_russian(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+
+    await call.message.answer("Этот раздел пока *в разработке*. Вы можете готовиться к другим предметам.",
+                              parse_mode="Markdown")
+
+
+@dp.callback_query_handler(text_contains="russian")
+async def choosing_russian(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+
+    await call.message.answer("Этот раздел пока *в разработке*. Вы можете готовиться к другим предметам.",
+                              parse_mode="Markdown")
+
+
+@dp.callback_query_handler(text_contains="russian")
+async def choosing_russian(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+
+    await call.message.answer("Этот раздел пока *в разработке*. Вы можете готовиться к другим предметам.",
+                              parse_mode="Markdown")
+
+
+@dp.callback_query_handler(text_contains="russian")
+async def choosing_russian(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+
+    await call.message.answer("Этот раздел пока *в разработке*. Вы можете готовиться к другим предметам.",
+                              parse_mode="Markdown")
+
+
+@dp.callback_query_handler(text_contains="russian")
+async def choosing_russian(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+
+    await call.message.answer("Этот раздел пока *в разработке*. Вы можете готовиться к другим предметам.",
+                              parse_mode="Markdown")
+
+
+@dp.callback_query_handler(text_contains="russian")
+async def choosing_russian(call: CallbackQuery):
+    await call.answer(cache_time=60)
+    callback_data = call.data
+    logging.info(f"call = {callback_data}")
+
+    await call.message.answer("Этот раздел пока *в разработке*. Вы можете готовиться к другим предметам.",
+                              parse_mode="Markdown")
 
 
 @dp.callback_query_handler(text_contains="russian")
--- keyboards/inline/choise_buttons.py
+++ keyboards/inline/choise_buttons.py
@@ -6,18 +6,26 @@ choice = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text='Русский язык', callback_data=subject_callback.new(subject='russian')),
             InlineKeyboardButton(text='Математика', callback_data="choose:math"),
-            # InlineKeyboardButton(text='Обществознание', callback_data=''),
-            # InlineKeyboardButton(text='Информатика', callback_data=''),
-            # InlineKeyboardButton(text='Биология', callback_data=''),
-            # InlineKeyboardButton(text='Физика', callback_data=''),
-            # InlineKeyboardButton(text='История', callback_data=''),
-            # InlineKeyboardButton(text='Иностранный язык', callback_data=''),
-            # InlineKeyboardButton(text='Химия', callback_data=''),
-            # InlineKeyboardButton(text='Литература', callback_data=''),
-            # InlineKeyboardButton(text='География', callback_data='')
         ],
         [
-            InlineKeyboardButton(text='Посмотреть свою статистику', callback_data='next')
+            InlineKeyboardButton(text='Обществознание', callback_data="choose:social"),
+            InlineKeyboardButton(text='Биология', callback_data="choose:biology"),
+        ],
+        [
+            InlineKeyboardButton(text='Физика', callback_data="choose:physics"),
+            InlineKeyboardButton(text='Информатика', callback_data="choose:computer"),
+        ],
+        [
+            InlineKeyboardButton(text='История', callback_data="choose:history"),
+            InlineKeyboardButton(text='Иностранный язык', callback_data="choose:foreign"),
+        ],
+        [
+            InlineKeyboardButton(text='Химия', callback_data="choose:chemistry"),
+            InlineKeyboardButton(text='Литература', callback_data="choose:literature"),
+        ],
+        [
+            InlineKeyboardButton(text='География', callback_data="choose:geography"),
+            InlineKeyboardButton(text='Личный кабинет', callback_data='stats')
         ]
     ]
 )
